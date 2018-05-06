4946a2f5b609c67d166d762a27ce4c4937c84352
lbeckman314 <lbeckman314@gmail.com>
2018-05-06 14:47:57 -0700

    added repeat script for music

---
+++ repeat.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# sources
+# https://askubuntu.com/questions/359855/how-to-detect-insertion-of-dvd-disc
+
+
+waitForDisk()
+{
+
+    # I'll try spinning. That's a good trick!
+    case $(($i % 4)) in
+            0 ) j="-" ;;
+            1 ) j="\\" ;;
+            2 ) j="|" ;;
+            3 ) j="/" ;;
+    esac
+
+    echo -en "\r[$j]\e[1;37m waiting for cd in /dev/cdrom...\e[m"
+
+    ((i=i+1))
+}
+
+
+i=0
+while [ true ]
+do
+    if mount | grep -q /dev/cdrom; then
+        echo -e "\e[1;36mcd detected, starting script...\e[m"
+        # sleep 3
+        ./music.sh
+        wait
+    else
+        waitForDisk
+    fi
+    sleep 0.5
+done
+
